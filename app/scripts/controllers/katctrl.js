5348fbe885e035bc684cfb7ee85d3bba19c3e47c
Katherine Mattison <kbmedisto@gmail.com>
2014-06-16 02:35:27 -0400

    zerodark30

---
+++ app/scripts/controllers/katctrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('britTrialNgApp')
+  .controller('katCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  })
